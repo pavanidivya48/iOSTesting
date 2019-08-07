79653a76f32323f700153db8eb5877da584358b9
pavanidivya <pavanidivya48@gmail.com>
2019-08-07 17:26:19 +0530

    Create test project via pod create lib.

---
+++ Example/Pods/Target Support Files/Pods-iOSTesting_Tests/Pods-iOSTesting_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_iOSTesting_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_iOSTesting_Tests
+@end
---
+++ Example/Pods/Target Support Files/iOSTesting/iOSTesting-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_iOSTesting : NSObject
+@end
+@implementation PodsDummy_iOSTesting
+@end
