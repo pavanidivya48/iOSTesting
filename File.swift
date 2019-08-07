2cb640c046fd8b013072deca62c8213a1a540e72
pavanidivya <pavanidivya48@gmail.com>
2019-08-07 18:08:41 +0530

    JustKidding

---
+++ File.swift
@@ -0,0 +1,22 @@
+//
+//  File.swift
+//  iOSTesting
+//
+//  Created by Ats on 07/08/19.
+//
+
+import Foundation
+
+public class RandomGenerator {
+    private init() { }
+    
+    public static func string() -> String {
+        return UUID().uuidString
+    }
+    
+    public static func integer() -> Int {
+        return Int(arc4random()) 
+    }
+    
+    
+}
